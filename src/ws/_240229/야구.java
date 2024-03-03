34bbd28e39be1a32dadfb604255cdc1c563d572f
dlwnstn0524 <ljs052450@gmail.com>
2024-03-03 19:49:06 +0900

    [FEAT] 0302 TIL

--- src/ws/_240229/야구.java
+++ src/ws/_240229/야구.java
@@ -3,7 +3,6 @@ package ws._240229;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 야구 {
@@ -32,13 +31,13 @@ public class 야구 {
 		v[0] = true;
 		sel = new int[9];
 		permu(0);
+		// simul(new int[]{4,5,6,0,1,2,3,7,8});
 
 		System.out.println(answer);
 	}
 
 	private static void permu(int depth) {
 		if (depth == 9) {
-			System.out.println(Arrays.toString(sel));
 			simul(sel);
 			return;
 		}
@@ -54,32 +53,69 @@ public class 야구 {
 				v[i] = false;
 			}
 		}
-
 	}
 
 	private static void simul(int[] order) {
 		int score = 0;
 		int idx = 0;
 		for (int i = 0; i < N; i++) {
+			
 			// 매회마다 베이스, 아웃카운트 초기화, 타자의 순서는 그대로 유지한다.
 			int out = 0;
 			boolean[] base = new boolean[3];
+			
 			while (out < 3) {
+				// System.out.println(Arrays.toString(base) + " " + out + " " + result[i][order[idx]]);
 				if (result[i][order[idx]] == 0) { // 아웃일때
 					out++;
 				} else {
-					for (int j = 1; j <= 4; j++) {
-						if (j == result[i][order[idx]]) { // 1루타, 2루타, 3루타, 4루타 -> 홈런
-							for (int k = 0; k < 3; k++) {
-								if(base[k] && k + j < 4) {
-									base[k+j] = true;
+					if(result[i][order[idx]] == 1){ // 1루타
+						for (int k = 2; k >= 0; k--) {
+                            if (base[k]) {
+                                if (k == 2) { // 3루에 있는 선수는 홈으로 들어옴.
+                                    score++; // 동시에 점수 획득.
+                                    base[k] = false; // 3루는 비어있게 됨.
+                                    continue;
+                                }
+ 
+                                // 1, 2루에 경우 1루씩 진루하고 원래 있던 자리는 비어있게 됨.
+                                base[k] = false;
+                                base[k + 1] = true;
+                            }
+                        }
+                        base[0] = true; // 홈에서 1루로 진루.
+					} else if(result[i][order[idx]] == 2){
+						for (int k = 2; k >= 0; k--) {
+                            if (base[k]) {
+                                if (k == 2 || k == 1) { // 3루 혹은 2루에 있는 선수는 홈으로 들어옴.
+                                    score++; // 동시에 점수 획득.
+                                    base[k] = false; // 2루 또는 3루는 비어있게 됨.
+                                } else {
+									// 1루에 경우 2루씩 진루하고 원래 있던 자리는 비어있게 됨.
 									base[k] = false;
-								} else if(base[k] && k + j > 4) {
-									base[k] = false;
-									score++;
+									base[k + 2] = true;
 								}
-							}
-						}
+                            }
+                        }
+                        base[1] = true; // 홈에서 2루로 진루.
+                        
+					} else if (result[i][order[idx]] == 3){
+						for (int k = 2; k >= 0; k--) {
+                            if (base[k]) { // 홈 제외 모든 선수는 홈으로 들어옴.
+                                score++; // 동시에 점수 획득.
+                                base[k] = false;
+                            }
+                        }
+                        base[2] = true; // 홈에서 3루로 진루.
+                        
+					} else if (result[i][order[idx]] == 4) {
+						for (int k = 2; k >= 0; k--) {
+                            if (base[k]) { // 홈 제외 모든 선수는 홈으로 들어옴.
+                                score++; // 동시에 점수 획득.
+                                base[k] = false;
+                            }
+                        }
+						score++;	
 					}
 				}
 				idx++;
