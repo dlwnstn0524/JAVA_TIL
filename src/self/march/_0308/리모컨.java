5de7842773f4731ef8e7c07b24065daf8a0e789f
dlwnstn0524 <ljs052450@gmail.com>
2024-03-09 00:46:11 +0900

    [ADD] 리모콘 풀이중

--- src/self/march/_0308/리모컨.java
+++ src/self/march/_0308/리모컨.java
@@ -1,9 +1,33 @@
 package self.march._0308;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class 리모컨 {
 
-	public static void main(String[] args) {
+	static String chanel;
+	static int N, answer;
+	static String[] error;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		chanel = br.readLine();
 		
+		N = Integer.parseInt(br.readLine());
+		
+		error = new String[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			error[i] = st.nextToken();	
+		}
+		answer = Integer.MAX_VALUE;
+		recursive("", );
+		System.out.println(answer);
+		System.out.println(Arrays.toString(error));
 	}
 
 }
