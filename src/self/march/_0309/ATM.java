9094c51e8d4bbce3af3c95de75aa5acd5779d4ee
dlwnstn0524 <ljs052450@gmail.com>
2024-03-09 11:20:46 +0900

    [ADD] ATM 풀이중

---
+++ src/self/march/_0309/ATM.java
@@ -0,0 +1,35 @@
+package self.march._0309;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ATM {
+    static int N, answer;
+    static int[] nums;
+    static int[] dp;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        dp = new int[N];
+        dp[0] = nums[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = dp[i-1] + nums[i];
+        }
+        answer = 0;
+        for (int i = 0; i < args.length; i++) {
+            answer += dp[i];
+        }
+        System.out.println(answer);
+    }
+}
