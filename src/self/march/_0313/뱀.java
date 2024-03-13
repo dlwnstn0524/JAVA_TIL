d2068a3fb27d20d9f933f6827585984c3a1cbb9d
dlwnstn0524 <ljs052450@gmail.com>
2024-03-13 17:54:44 +0900

    뱀 풀이중

--- src/self/march/_0313/뱀.java
+++ src/self/march/_0313/뱀.java
@@ -3,6 +3,8 @@ package self.march._0313;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 뱀 {
@@ -14,11 +16,13 @@ public class 뱀 {
             this.dir = d;
         }
     }
-    static int N, K, L, answer;
+    static int N, K, L, dir, answer;
     static int[][] maps;
     static int[] di = {0, 1, 0, -1};
     static int[] dj = {1, 0, -1, 0};
     static Change[] changes;
+    static Queue<int []> q;
+    
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -41,5 +45,34 @@ public class 뱀 {
             char d = st.nextToken().charAt(0);
             changes[i] = new Change(t, d);
         }
-    } 
+        
+        maps[0][0] = 1;
+        q = new ArrayDeque<>();
+        q.offer(new int[] {0, 0});
+        
+        answer = 0;
+        dir = 0;
+        while(true) {
+        	answer++;
+        	int[] cur = q.poll();
+        	int ni = cur[0] + di[dir];
+        	int nj = cur[1] + dj[dir];
+        	if(ni < 0 || ni >= N || nj < 0 || nj >= N || maps[ni][nj] == 1) break;
+        	else {
+        		if(maps[ni][nj] == 2) q.offer(new int[] {});
+        	}
+        	
+        }
+        System.out.println(answer);
+        print(maps);
+    }
+
+	private static void print(int[][] maps2) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(maps[i][j] + " ");
+			}
+			System.out.println();
+		}
+	} 
 }
